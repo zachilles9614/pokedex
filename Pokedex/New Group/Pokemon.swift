523abbc29879056a9aa957348448fa0f9f39cdba
Zachary Auer <auerzachary@yahoo.com>
2017-10-08 23:11:58 -0500

    setting up the background for the app and adding the pokemon object. Setting up collection view.

---
+++ Pokedex/New Group/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Zachary Auer on 10/8/17.
+//  Copyright © 2017 Zachary Auer. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokeDexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
